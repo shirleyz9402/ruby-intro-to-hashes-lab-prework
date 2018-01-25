b7e264730121aaf603cffc17132836fc5a54fce1
Shirley Zhang <shirleyz9402@yahoo.com>
2018-01-25 01:17:01 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,7 +31,7 @@ def monopoly_with_third_tier
     :pieces => 4,
     :names => {
       :reading_railroad => {
-        
+         "mortgage_value"=>"$100"
       },
       :pennsylvania_railroad => {
         
